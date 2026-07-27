b591b0d3c12e41f704e9892936c391bd629d4aff
Aadarsha Dumre <adarshadumre45@gmail.com>
2026-07-27 18:40:30 +0545

    email send correction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link, Navigate } from "react-router-dom";
 import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 import {
   ArrowRight,
   Monitor,
@@ -24,6 +25,7 @@ import {
   FaTwitter,
 } from "react-icons/fa";
 import LandingPageCourse from "../components/LandingPageCourses";
+import { toast } from "sonner";
 
 
 export default function Home() {
@@ -46,6 +48,11 @@ export default function Home() {
     e.preventDefault();
 
     try {
+
+      if(!name || !email || !subject || !message){
+        toast.error("all fields are required")
+        return
+      }
       await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
@@ -58,7 +65,7 @@ export default function Home() {
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       );
 
-      alert("Message sent successfully!");
+      toast.success("Message sent successfully!");
 
       setFormData({
         name: "",
@@ -67,9 +74,9 @@ export default function Home() {
         message: "",
       });
     } catch (error) {
-      console.error(error);
-      alert("Failed to send message.");
-    }
+  console.error("EmailJS Error:", error);
+  toast.error(error.text || error.message || "Failed to send message.");
+}
   };
 
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
